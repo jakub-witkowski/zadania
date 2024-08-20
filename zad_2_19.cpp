81a8c18c110e0d3f75c5ea9339c3035f3a1ab486
jakub.witkowski <j.witkowski@poczta.onet.pl>
2024-08-20 22:57:06 +0200

    further exercise files

--- zad_2_19.cpp
+++ zad_2_19.cpp
@@ -19,12 +19,29 @@ void draw_left_aligned_right_triangle(int n)
     }
 }
 
+void draw_right_aligned_right_triangle(int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            if (j >= n - i - 1)
+                std::cout << "# ";
+            else
+                std::cout << "  ";
+        }
+        
+        std::cout << std::endl;
+    }
+}
+
 int main()
 {
     int random = draw_number(5,12);
     std::cout << "Wylosowana liczba: " << random << ". \n";
 
     draw_left_aligned_right_triangle(random);
+    draw_right_aligned_right_triangle(random);
 
     return 0;
 }
